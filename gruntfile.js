33cf19b31ac2d732a13b4fa719c13b5085b98772
jtenner <tenner.joshua@gmail.com>
2016-03-31 11:34:14 -0400

    Fix all the things release v3.2.1

--- gruntfile.js
+++ gruntfile.js
@@ -1,23 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    bump: {
-      options: {
-        files: ['package.json','bower.json'],
-        updateConfigs: [],
-        commit: true,
-        commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json'],
-        createTag: true,
-        tagName: 'v%VERSION%',
-        tagMessage: 'Version %VERSION%',
-        push: false,
-        pushTo: 'upstream',
-        gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-        globalReplace: false,
-        prereleaseName: false,
-        regExp: false
-      }
-    },
+
     browserify: {
       dist: {
         src: ['./index.js'],
@@ -37,9 +20,8 @@ module.exports = function(grunt) {
       }
     }
   });
-  
+
   grunt.loadNpmTasks('grunt-browserify');
-  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.registerTask('default', ['browserify', 'uglify', 'bump']);
+  grunt.registerTask('default', ['browserify', 'uglify']);
 };
