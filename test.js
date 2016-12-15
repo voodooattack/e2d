9cb9f6b51f297c86358e9dbeff90159f101b2c80
jtenner <tenner.joshua@gmail.com>
2016-12-15 13:32:58 -0500

    add a test.js file, add benchmark.js devDependency

---
+++ test.js
@@ -0,0 +1,41 @@
+let benchmark = require('benchmark');
+let suite = new benchmark.Suite();
+
+let data = new Float64Array(606);
+for(let i = 0; i < 600; i++) {
+  data[i] = i;
+}
+
+let matrix = new Float64Array()
+let a = 0, b = 0, c = 0, d = 0, e = 0, f = 0;
+
+suite.add('Matrix#manual',
+  () => {
+    for (let i = 6; i <= 600; i+=6) {
+      matrix[0] = data[i - 6];
+      matrix[1] = data[i - 5];
+      matrix[2] = data[i - 4];
+      matrix[3] = data[i - 3];
+      matrix[4] = data[i - 2];
+      matrix[5] = data[i - 1];
+    }
+  }
+)
+.add('Matrix#destructure',
+  () => {
+    for (let i = 6; i <= 600; i+=6) {
+      [a, b, c, d, e, f] = data.subarray(i - 6, i);
+    }
+  }
+)
+.add('Matrix#copyWithin', 
+  () => {
+    for (let i = 6; i <= 600; i+=6) {
+      data.copyWithin(0, i, i+6);
+    }
+  }
+)
+.on('complete', function() {
+  console.log(this.filter('fastest'));
+})
+.run({ async: true });
